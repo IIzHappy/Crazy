ba18af5ba00ceaa9326f349426742974a1a5644a
PotatoSpyware <seansteven7@icloud.com>
2025-09-30 21:59:55 -0400

    Added Timer

--- Assets/Scripts/GameTime.cs
+++ Assets/Scripts/GameTime.cs
@@ -1,16 +1,28 @@
+using TMPro;
 using UnityEngine;
 
 public class GameTime : MonoBehaviour
 {
     //add timer
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float timer = 0;
+    private float minutes = 0;
+    private float seconds = 0;
+
+    [SerializeField] public TMP_Text time;
+
+    // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
+    // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime;
+        minutes = Mathf.Floor(timer / 60);
+        seconds = timer % 60;
+        time.text = string.Format("{0:0}:{1:00.00}", minutes, seconds);
     }
 }
